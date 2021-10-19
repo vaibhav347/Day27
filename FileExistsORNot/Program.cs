d70a1738b697c941b662afaa1c12b8c0c063c7e6
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-19 09:29:50 +0530

    ok

---
+++ FileExistsORNot/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FileExistsORNot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UseCase1 obj = new UseCase1();
+            obj.fileExists();
+        }
+    }
+}
---
+++ FileExistsORNot/UseCase1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace FileExistsORNot
+{
+    public class UseCase1
+    {
+        public void fileExists()
+        {
+            string path = "C:/Day27/FileExistsORNot/vaibhav.txt";
+            if (File.Exists(path))
+                Console.WriteLine("File is exists");
+            else
+                Console.WriteLine("File not exists");
+
+        }
+    }
+}
