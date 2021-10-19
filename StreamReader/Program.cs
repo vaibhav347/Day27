eb8dfb1f249866df9e5bb5fec38b9682c3b47664
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-19 09:33:35 +0530

    ok

---
+++ StreamReader/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+
+namespace Stream_Reader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            reader();
+        }
+        public static void reader()
+        {
+            string path = "C:/Day27/StreamReader/aher.txt";
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+
+        }
+    }
+}
