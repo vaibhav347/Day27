0ad7271b7f1b73716214b52bb618dd3e263d7696
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-19 09:30:34 +0530

    ok

---
+++ ReadFileByText/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace ReadFileByText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            read();
+        }
+        public static void read()
+        {
+            string path = "C:/Day27/ReadFileByText/vaibhav.txt";
+            Console.WriteLine(File.ReadAllText(path));
+        }
+    }
+}
