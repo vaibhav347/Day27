3b64af9684dbc2bbf2686abdf852d904d3dfe0e8
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-19 09:31:09 +0530

    ok

---
+++ ReadFileByAllLines/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace ReadFileByAllLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            read();
+        }
+        public static void read()
+        {
+            string path = "C:/Day27/ReadFileByAllLines/vaibhav.txt";
+            string[] line;
+            line = File.ReadAllLines(path);
+            Console.WriteLine(line[0]);
+
+        }
+    }
+}
