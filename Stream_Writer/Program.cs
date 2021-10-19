ecab78cde1df234f682eab0771086d95fa9b6d96
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-19 09:34:20 +0530

    ok

---
+++ Stream_Writer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace Stream_Writer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            writer();
+        }
+        public static void writer()
+        {
+            string path = @"C:/Day27/Stream_Writer/aher.txt";
+            using (StreamWriter sw = File.AppendText(path))
+            {
+
+                sw.WriteLine("Hello! How are you?");
+                sw.Close();
+
+            }
+        }
+    }
+}
