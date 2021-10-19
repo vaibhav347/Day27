7bf9c5f3226d775b6902ec17cc9a22b5c58c019d
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-19 09:32:21 +0530

    ok

---
+++ MoveFile/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+namespace MoveFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            moveFile();
+        }
+        public static void moveFile()
+        {
+            string source_File = "C:/Day27/MoveFile/vaibhav.txt";
+            string dest_File = "C:/Day27/MoveFile/aher.txt";
+            File.Move(source_File, dest_File);
+        }
+    }
+}
