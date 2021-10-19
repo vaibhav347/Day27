b3f57d5f217934e3f066467d410068833731271f
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-19 09:32:52 +0530

    ok

---
+++ DeleteFile/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+namespace DeleteFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public static void moveFile()
+        {
+            string path = "C:/Day27/MoveFile/vaibhav.txt";
+            File.Delete(path);
+        }
+
+    }
+}
