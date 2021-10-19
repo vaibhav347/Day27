f684b00a59e3dcd54c465e859d340471be0203b3
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-19 09:31:46 +0530

    ok

---
+++ CopyFile/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+namespace CopyFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            copyFile();
+        }
+        public static void copyFile()
+        {
+            string source_File = "C:/Day27/CopyFile/vaibhav.txt";
+            string dest_File = "C:/Day27/CopyFile/aher.txt";
+            File.Copy(source_File, dest_File);
+        }
+    }
+}
